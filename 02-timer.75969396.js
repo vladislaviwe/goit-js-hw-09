6d7dd499fde0780f3bdbd0cebc1eaaf14a27e5de
vladislaviwe <93213929+vladislaviwe@users.noreply.github.com>
2022-08-04 17:24:45 +0000

    Deploying to gh-pages from @ vladislaviwe/goit-js-hw-09@e949e646184ddce345d636ad0824069ccdebb0ca 🚀

--- 02-timer.75969396.js
+++
@@ -1,2 +0,0 @@
-const e={timer:document.querySelector(".timer"),fieldValue:document.querySelectorAll(".value"),fieldLabel:document.querySelectorAll(".label")};e.timer.style.display="flex",e.fieldValue.forEach((e=>{e.style.cssText="font-size: 20px; font-weight: bold; color: tomato;"})),e.fieldLabel.forEach((e=>{e.style.cssText="margin-right: 5px"}));
-//# sourceMappingURL=02-timer.75969396.js.map
