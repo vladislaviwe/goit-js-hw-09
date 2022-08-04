e949e646184ddce345d636ad0824069ccdebb0ca
vladislaviwe <vladislaviwe@outlook.com>
2022-08-04 20:24:00 +0300

    Task 2 in progress

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,8 +1,13 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
 const refs = {
     timer: document.querySelector(".timer"),
+    dateTimePicker: document.querySelector("#datetime-picker"),
     fieldValue: document.querySelectorAll(".value"),
     fieldLabel: document.querySelectorAll(".label"),
-}
+    startButton: document.querySelector("button[data-start]"),
+};
 
 refs.timer.style.display = "flex";
 refs.fieldValue.forEach(e => {
@@ -12,4 +17,23 @@ refs.fieldValue.forEach(e => {
 refs.fieldLabel.forEach(e => {
     e.style.cssText = 'margin-right: 5px';
 });
+refs.startButton.disabled = true;
 
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+      console.log(selectedDates[0]);
+      
+      if (selectedDates[0] <= new Date()) {
+          alert('You entered incorrect data. Please enter a date in the future!');
+          refs.startButton.disabled = true;
+      } else {
+          refs.startButton.disabled = false;
+      };
+  },
+};
+
+flatpickr(refs.dateTimePicker, options);
